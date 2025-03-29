8683ea93a60516c6c0e72e028214c1de38e4b9bb
Hoang Anh <doviethoanganh07@gmail.com>
2025-03-29 19:20:13 +0700

    Update 1.1.py

--- 1.1.py
+++ 1.1.py
@@ -43,7 +43,6 @@ formatted_device_id = format_device_id(random_id)
 print("\033[1;97m╔═════════════════╗\033[0m")
 print("\033[1;97m║ \033[1;96mSpam SMS + Call \033[1;97m║\033[0m")
 print("\033[1;97m╚═════════════════╝\033[0m")
-print('\033[1;97m════════════════════════════════════════════════════════\033[0m')
 
 def send_otp_via_sapo(sdt):
     cookies = {
