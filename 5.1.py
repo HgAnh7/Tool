c7bd67f37d66312e39fd3b4aa39eb95c5df07666
Hoang Anh <doviethoanganh07@gmail.com>
2025-03-30 14:58:22 +0700

    Update 5.1.py

--- 5.1.py
+++ 5.1.py
@@ -61,5 +61,5 @@ while True:
         time.sleep(1.5)
         break
     else:
-        for _ in range(40):  
+        for _ in range(21):  
             print('\033[A\033[K', end='')  # Di chuyển lên & xóa dòng
