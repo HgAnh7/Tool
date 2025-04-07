0b13c349debdeceead99105c1341888cd33cb46c
Hoang Anh <doviethoanganh07@gmail.com>
2025-04-07 12:39:30 +0700

    Update ddos_web.py

--- ddos_web.py
+++ ddos_web.py
@@ -1,10 +1,10 @@
 # Share By @HgAnh_7
 
+import os
+import time
+import random
 import requests
 import threading
-import random
-import time
-import os
 
 USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
